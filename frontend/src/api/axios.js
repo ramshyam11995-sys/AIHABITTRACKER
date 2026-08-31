094f930ee1eb7f6a442cb543e8e515b4bb5296d0
ramshyam11995-sys <ramshyam11995@gmail.com>
2026-08-31 10:11:57 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
   import axios from "axios";
 
   const api = axios.create({
-    baseURL: https://aihabittracker-r7ku.onrender.com,
+    baseURL: "https://aihabittracker-r7ku.onrender.com/api",
   });
 
   api.interceptors.request.use((config) => {
